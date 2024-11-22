4591ddd5f9d63df181eb45c8b06b93d304e39d60
STI <STI@LAB3-NUC-89>
2024-11-22 12:13:22 +0800

    Add project files.

---
+++ frmTrackThread(Abenis)/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmTrackThread_Abenis_
+{
+    public partial class Form1 : Form
+    {
+        Thread threadA, threadB, threadC, threadD;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "-THREAD STARTS-";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Thread Starts-");
+            ThreadStart firstMainThread = new ThreadStart(MyThreadClass.Thread1);
+            Console.WriteLine("Thread Sequence 1:");
+            threadA = new Thread(firstMainThread);
+            threadA.Name = "Thread A";
+            threadA.Priority = ThreadPriority.Highest;
+
+            threadB = new Thread(firstMainThread);
+            threadB.Name = "Thread B";
+            threadA.Priority = ThreadPriority.Normal;
+
+            threadC = new Thread(firstMainThread);
+            threadC.Name = "Thread C";
+            threadA.Priority = ThreadPriority.AboveNormal;
+
+            threadD = new Thread(firstMainThread);
+            threadD.Name = "Thread D";
+            threadA.Priority = ThreadPriority.BelowNormal;
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            ThreadStart secondMainThread = new ThreadStart(MyThreadClass.Thread2);
+            Console.WriteLine("Thread Sequence 2:");
+            threadA = new Thread(secondMainThread);
+            threadA.Name = "Thread A";
+            threadA.Priority = ThreadPriority.Highest;
+
+            threadB = new Thread(secondMainThread);
+            threadB.Name = "Thread B";
+            threadA.Priority = ThreadPriority.Normal;
+
+            threadC = new Thread(secondMainThread);
+            threadC.Name = "Thread C";
+            threadA.Priority = ThreadPriority.AboveNormal;
+
+            threadD = new Thread(secondMainThread);
+            threadD.Name = "Thread D";
+            threadA.Priority = ThreadPriority.BelowNormal;
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+        }
+    }
+}
---
+++ frmTrackThread(Abenis)/MyThreadClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace frmTrackThread_Abenis_
+{
+    public class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            int loopCount;
+            for(int i = 1; i <= 2; i++)
+            {
+                loopCount = i;
+                Thread.Sleep(500);
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+            }
+        }
+        public static void Thread2()
+        {
+            int loopCount;
+            for (int i = 1; i <= 6; i++)
+            {
+                loopCount = i;
+                Thread.Sleep(1500);
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+            }
+        }
+    }
+}
